0e688e1d227b1c61b26d73d293f84adc77fe2e0a
Ubuntu <yutingy@ShaderML12.ylbntlmaklsuthliuxw3fdnw3d.bx.internal.cloudapp.net>
2020-06-08 13:49:53 +0000

    update code

--- CAN24_AN/defocus_blur_postprocessing.py
+++ CAN24_AN/defocus_blur_postprocessing.py
@@ -6,11 +6,19 @@ import numpy as np
 import glob
 import skimage
 import skimage.io
+import shutil
 
 dir = 'mandelbulb_defocus_blur_none_normal_none_test'
 kernels = [(3, 3), (5, 5), (9, 9), (17, 17), (33, 33)]
 t_dir = '/n/fs/shaderml/1x_1sample_mandelbulb_tile/train/mandelbulb_defocus_blur_none_normal_none'
 prefix = 'test_middle_ground'
+
+dir = sys.argv[1]
+t_dir = sys.argv[2]
+prefix = sys.argv[3]
+intermediate_dir = sys.argv[4]
+target_img_dir = sys.argv[5]
+target_sigma_dir = sys.argv[6]
 
 def generate_blurred_imgs():
     files = os.listdir(dir)
@@ -22,7 +30,7 @@ def generate_blurred_imgs():
                 kernel = kernels[i]
                 sigma = 0.3 * ((kernel[0] - 1) * 0.5 - 1) + 0.8
                 blurred_img = cv2.GaussianBlur(in_img, kernel, sigma)
-                cv2.imwrite(os.path.join(dir, imgname + '_blur_%d.png' % i), blurred_img)
+                cv2.imwrite(os.path.join(intermediate_dir, imgname + '_blur_%d.png' % i), blurred_img)
 
 def compute_sigma():
     nframes = len(glob.glob(os.path.join(t_dir, 'g_intermediates*')))
@@ -41,6 +49,12 @@ def compute_sigma():
 
         tmax = numpy.max(t_ray)
         tmin = numpy.min(t_ray)
+        
+        if tmax == tmin:
+            shutil.copyfile(os.path.join(dir, '%s%05d.png' % (prefix, i)), os.path.join(target_img_dir, 'defocus_blur_%s%05d.png') % (prefix, i))
+            numpy.save(os.path.join(target_sigma_dir, 'sigma%s%05d.npy' % (prefix, i)), np.zeros(t_ray.shape))
+            continue
+        
         t_ray -= tmin
         t_ray /= (tmax - tmin)
         print(i)
@@ -50,8 +64,8 @@ def compute_sigma():
 
 
         current_sigma = t_ray * sigma_max
-        numpy.save(os.path.join(t_dir, 'sigma%05d.npy' % i), current_sigma / numpy.max(current_sigma))
-        continue
+        #numpy.save(os.path.join(target_sigma_dir, 'sigma%05d.npy' % i), current_sigma / numpy.max(current_sigma))
+        
         coefs = [None] * sigmas.shape[0]
 
         for ind in range(sigmas.shape[0]):
@@ -66,16 +80,16 @@ def compute_sigma():
                 img = skimage.img_as_float(skimage.io.imread(os.path.join(dir, '%s%05d.png' % (prefix, i))))
                 ans = img * numpy.expand_dims(coefs[ind], axis=2)
             else:
-                img = skimage.img_as_float(skimage.io.imread(os.path.join(dir, '%s%05d_blur_%d.png' % (prefix, i, ind - 1))))
+                img = skimage.img_as_float(skimage.io.imread(os.path.join(intermediate_dir, '%s%05d_blur_%d.png' % (prefix, i, ind - 1))))
                 ans += img * numpy.expand_dims(coefs[ind], axis=2)
 
-        skimage.io.imsave(os.path.join(dir, 'defocus_blur_%s%05d.png') % (prefix, i), numpy.clip(ans, 0, 1))
+        skimage.io.imsave(os.path.join(target_img_dir, 'defocus_blur_%s%05d.png') % (prefix, i), numpy.clip(ans, 0, 1))
 
         # save for range 0 - 1
         current_sigma /= numpy.max(current_sigma)
-        numpy.save(os.path.join(t_dir, 'sigma%05d.npy' % i), current_sigma)
+        numpy.save(os.path.join(target_sigma_dir, 'sigma%s%05d.npy' % (prefix, i)), current_sigma)
 
 
 if __name__ == '__main__':
-    #generate_blurred_imgs()
+    generate_blurred_imgs()
     compute_sigma()
