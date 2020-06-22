040fe375d0af3a1e3ec555716867abd984ce3e42
Ubuntu <yutingy@ShaderML4.ylbntlmaklsuthliuxw3fdnw3d.bx.internal.cloudapp.net>
2020-06-22 14:56:15 +0000

    update code

--- CAN24_AN/copy_without_meta.py
+++ CAN24_AN/copy_without_meta.py
@@ -9,12 +9,17 @@ for root, dirs, files in os.walk(src_dir):
     for file in files:
         if not file.endswith('.meta'):
             src_file = os.path.join(root, file)
-            dst_file = os.path.join(dst_dir, src_file.replace(src_dir, ''))
             
-            dst_dir, _ = os.path.split(dst_file)
+            current_src_dir = src_file.replace(src_dir, '')
+            if current_src_dir.startswith('/'):
+                current_src_dir = current_src_dir[1:]
             
-            if not os.path.isdir(dst_dir):
-                os.makedirs(dst_dir)
+            dst_file = os.path.join(dst_dir, current_src_dir)
+            
+            current_dst_dir, _ = os.path.split(dst_file)
+            
+            if not os.path.isdir(current_dst_dir):
+                os.makedirs(current_dst_dir)
             
             shutil.copyfile(src_file, dst_file)
             
